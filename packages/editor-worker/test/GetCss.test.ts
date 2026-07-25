aac788ad88152ecc558b36298aa588ee392d5b0b
Le Vivilet <levivilet@pm.me>
2026-07-25 11:13:25 +0000

    fix: update cursor css integration tests

--- packages/editor-worker/test/GetCss.test.ts
+++ packages/editor-worker/test/GetCss.test.ts
@@ -4,6 +4,8 @@ import { getCss } from '../src/parts/GetCss/GetCss.ts'
 
 test('renders dynamic cursor positions for each selection', () => {
   const state: EditorState = {
+    canRedo: false,
+    canUndo: false,
     columnWidth: 9,
     diagnostics: [],
     findWidgetVisible: false,
@@ -15,14 +17,18 @@ test('renders dynamic cursor positions for each selection', () => {
     longestLineWidth: 99,
     maxLineY: 2,
     minLineY: 0,
+    modified: false,
     rowHeight: 20,
     scrollBarWidth: 0,
+    scrollLeft: 0,
+    scrollTop: 0,
     selections: new Uint32Array([1, 5, 1, 5, 0, 2, 0, 3]),
     tokenizedLines: [],
     tokenizePath: '',
     uid: 0.5,
     uri: 'file:///test.txt',
     useCache: true,
+    version: 0,
     width: 300,
     x: 100,
     y: 50,
