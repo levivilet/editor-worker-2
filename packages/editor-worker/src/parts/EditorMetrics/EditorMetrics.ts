a7bb44ce7367461c13ba88969f5a464a9f72a9d6
Le Vivilet <levivilet@pm.me>
2026-07-25 08:43:58 +0000

    fix: improve cursor position accuracy

--- packages/editor-worker/src/parts/EditorMetrics/EditorMetrics.ts
+++ packages/editor-worker/src/parts/EditorMetrics/EditorMetrics.ts
@@ -1,2 +1,8 @@
 export const ColumnWidth = 9
+export const FontFamily = 'Fira Code'
+export const FontSize = 15
+export const FontWeight = 400
+export const IsMonospaceFont = true
+export const LetterSpacing = 0.5
 export const RowHeight = 20
+export const TabSize = 2
