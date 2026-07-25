22e4fdc7c2c576545c57607bd891b10c6d8940b1
Le Vivilet <levivilet@pm.me>
2026-07-25 17:52:07 +0000

    test: defer integrated find widget e2e

--- packages/e2e/src/find-widget.close.ts
+++ packages/e2e/src/find-widget.close.ts
@@ -1,6 +1,7 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'find-widget.close'
+export const skip = 1
 
 export const test: Test = async ({ Editor, expect, FileSystem, FindWidget, Locator, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
