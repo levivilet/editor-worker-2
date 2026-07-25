26a9a853fbf5be5969a1ec2de5a0c5dd11bd5d48
Le Vivilet <levivilet@pm.me>
2026-07-25 06:43:55 +0200

    feature: connect editor worker services (#2)

--- packages/editor-worker/src/parts/Listen/Listen.ts
+++ packages/editor-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,32 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { LazyTransferMessagePortRpcParent, WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { ClipBoardWorker, ExtensionManagementWorker, FileSystemWorker, RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 
+export const initializeWorkerConnections = async (): Promise<void> => {
+  const [fileSystemRpc, extensionManagementRpc, clipBoardRpc] = await Promise.all([
+    LazyTransferMessagePortRpcParent.create({
+      commandMap: {},
+      send: (port) => RendererWorker.sendMessagePortToFileSystemWorker(port, RpcId.EditorWorker),
+    }),
+    LazyTransferMessagePortRpcParent.create({
+      commandMap: {},
+      send: (port) => RendererWorker.sendMessagePortToExtensionManagementWorker(port, RpcId.EditorWorker),
+    }),
+    LazyTransferMessagePortRpcParent.create({
+      commandMap: {},
+      send: (port) => RendererWorker.sendMessagePortToClipBoardWorker(port, RpcId.EditorWorker),
+    }),
+  ])
+
+  FileSystemWorker.set(fileSystemRpc)
+  ExtensionManagementWorker.set(extensionManagementRpc)
+  ClipBoardWorker.set(clipBoardRpc)
+}
+
 export const listen = async (): Promise<void> => {
-  await WebWorkerRpcClient.create({
+  const rendererRpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
+  RendererWorker.set(rendererRpc)
+  await initializeWorkerConnections()
 }
--- packages/editor-worker/test/Listen.test.ts
+++ packages/editor-worker/test/Listen.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@jest/globals'
 import { mockWorkerGlobalRpc } from '@lvce-editor/rpc'
+import { ClipBoardWorker, ExtensionManagementWorker, FileSystemWorker, get, RendererWorker, RpcId } from '@lvce-editor/rpc-registry'
 import * as Listen from '../src/parts/Listen/Listen.ts'
 
 test('starts the editor worker rpc', async () => {
@@ -8,4 +9,36 @@ test('starts the editor worker rpc', async () => {
   start()
   await expect(listenPromise).resolves.toBeUndefined()
   dispose()
+})
+
+test('initializes lazy connections to the file system, extension management, and clipboard workers', async () => {
+  using mockRendererRpc = RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToClipBoardWorker'() {},
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionManagementWorker'() {},
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToFileSystemWorker'() {},
+  })
+
+  await Listen.initializeWorkerConnections()
+  expect(mockRendererRpc.invocations).toEqual([])
+
+  const clipBoardRpc = get(RpcId.ClipBoardWorker)
+  const extensionManagementRpc = get(RpcId.ExtensionManagementWorker)
+  const fileSystemRpc = get(RpcId.FileSystemWorker)
+  fileSystemRpc.send('test')
+  extensionManagementRpc.send('test')
+  clipBoardRpc.send('test')
+  await new Promise((resolve) => setTimeout(resolve, 0))
+
+  expect(mockRendererRpc.invocations).toEqual([
+    ['SendMessagePortToExtensionHostWorker.sendMessagePortToFileSystemWorker', expect.anything(), 'FileSystem.handleMessagePort', RpcId.EditorWorker],
+    [
+      'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionManagementWorker',
+      expect.anything(),
+      'Extensions.handleMessagePort',
+      RpcId.EditorWorker,
+    ],
+    ['SendMessagePortToExtensionHostWorker.sendMessagePortToClipBoardWorker', expect.anything(), 'ClipBoard.handleMessagePort', RpcId.EditorWorker],
+  ])
+
+  await Promise.all([ClipBoardWorker.dispose(), ExtensionManagementWorker.dispose(), FileSystemWorker.dispose()])
 })
